dc114cdc29ca9d3ce421ea63ad1b1d89abe9c618
khaled6013 <kmd601384@gmail.com>
2026-01-11 20:02:19 +0600

    user list

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <User users={users} />
+      <User users={users} setUsers={setUsers} />
     </>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,22 +1,43 @@
-const User = ({ users }) => {
+const User = ({ users, setUsers }) => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const name = e.target.name.value
+    const email = e.target.email.value
+    const userAdd = { name, email }
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userAdd)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setUsers([...users, data])
+        e.target.reset()
+      })
+  }
+
   return (
     <>
       <h1>User List</h1>
-      <form action="">
-        <input type="text" placeholder="name"/>
-        <br />
-        <br />
-        <input type="email" placeholder="email" />
-        <br />
-        <br />
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="name" required />
+        <br /><br />
+        <input type="email" name="email" placeholder="email" required />
+        <br /><br />
         <input type="submit" value="Add User" />
       </form>
+
       {
         users.map(user => (
-          <p key={user.id}>{user.name}: {user.email}</p>
+          <p key={user.id}>
+            {user.name}: {user.email}
+          </p>
         ))
       }
-
     </>
   )
 }
